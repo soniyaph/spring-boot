57a7cdb8c724d7d58b1207f8c6f1517f8014c40c
soniyaph <soniyaphuge24@gmail.com>
2021-07-27 19:43:00 +0530

    small chnages

--- src/main/java/com/spring/boot/DemoApplication.java
+++ src/main/java/com/spring/boot/DemoApplication.java
@@ -2,10 +2,17 @@ package com.spring.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
+	@RequestMapping("/message")
+	public String getMessage(){
+		return "Hello from Docker";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
